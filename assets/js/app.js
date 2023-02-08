56853a0173b6ef54d29393b0b5bab783e7dc2b4b
SriDini <123217221+dineshdevdesign@users.noreply.github.com>
2023-02-08 23:44:22 +0530

    01 Console

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+/*
+    projectJavaScript
+    08FEB2023 
+    v1.1
+    #git
+*/
+
+// alert('Hello World')
+
+// Log to console
+console.log('Hello World!');                    // string
+console.log(2212);                              // numbers
+console.log(true);                              // boolean #true / #false
+var welcome = 'Hi JavaScript';                  // variable
+console.log(welcome);
+console.log([1,2,3,4]);                         // Array of numbers
+console.log({a:1, b:2});                        // Objects literal
+console.table({a:1, b:2})                       // table
+
+console.error('This is error');                 // error
+console.clear();                                // clear
+console.warn('This is warning');                // warning
+
+console.time('Hello');                          // time Identify 
+    console.log('Hello World!');                // start point
+    console.log('Hello World!');
+    console.log('Hello World!');
+    console.log('Hello World!');                    // Load Time will display
+    console.log('Hello World!');
+    console.log('Hello World!');
+    console.log('Hello World!');                // end point
+console.timeEnd('Hello')                        // timeEnd
